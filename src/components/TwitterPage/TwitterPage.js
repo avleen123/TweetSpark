10b4a9a2e8e05045ef6a03b28e3e60b6de1fa6f4
avleen <mehak.bakshi201@gmail.com>
2023-03-23 22:22:38 +0530

    Generation of suggested prompts💬

--- src/components/TwitterPage/TwitterPage.js
+++ src/components/TwitterPage/TwitterPage.js
@@ -1,32 +1,81 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Copy } from 'react-feather';
 import styles from './TwitterPage.module.css';
 import TwitterCard from "../TwitterCard.js/TwitterCard";
 import { generateImageForTweet, generatePrompts } from '../../apis/twitter';
 function TwitterPage() {
-  useEffect(()=>{
-   generateImageForTweet('Earth is flat')
-  },[]);
+  const [prompts, setPrompts] = useState([]);
+  const [promptInput,setPromptInput]=useState("");
+  const [errorMessages, setErrorMessages] = useState({
+    prompt: "",
+    thread: "",
+    image: "",
+  });
+  const [disabledButtons, setDisabledButtons] = useState({
+    prompt: false,
+    thread: false,
+    image: false,
+  });
+  const handlePromptGeneration = async () => {
+    if (!promptInput.trim()) {
+      setErrorMessages((prev) => ({
+        ...prev,
+        prompt: "Please enter something",
+      }));
+      return;
+    }
+    setErrorMessages((prev) => ({
+      ...prev,
+      prompt: "",
+    }));
+    setPrompts([]);
+    setDisabledButtons((prev) => ({ ...prev, prompt: true }));
+    const res = await generatePrompts(promptInput);
+    setDisabledButtons((prev) => ({ ...prev, prompt: false }));
+    if (!res) {
+      setErrorMessages((prev) => ({
+        ...prev,
+        prompt: "Error getting the prompts, please try again",
+      }));
+      return;
+    }
+    const tweets = res.choices.map((item) => item.text.trim());
+    setPrompts(tweets);
+  };
+  useEffect(()=>{},[]);
   return (
     <div className={styles.container}>
     <div className={styles.mainLeft}>
       <div className={styles.topSection}>
         <label>What's in your mind today ?</label>
-        <textarea placeholder="Writer here..."/>
-        <button className='button'>Generate Prompts</button>
+        <textarea placeholder="Writer here..."
+        value={promptInput}
+        onChange={(event)=> setPromptInput(event.target.value)}
+        />
+        <button className='button' onClick={handlePromptGeneration} disabled={disabledButtons.prompt}>
+        {disabledButtons.prompt ? "Generating..." : "Generate prompts"}
+          </button>
         </div>
         <div className={styles.prompts}>
-        <div className={styles.prompt}>
-         <p className={styles.text}>This is some text.</p>
-         <div className={styles.bottom}>
-          <div className='icon'>
-            <Copy/>
-          </div>
-          </div> 
+          <p className={styles.heading}>Suggested prompts</p>
+          {disabledButtons.prompt ? (
+            <p>Loading...</p>
+          ) : prompts.length == 0 ? (
+            <p>No prompts to show, generate some new prompts. </p>
+          ) : (
+            prompts.map((item) => (
+              <div className={styles.prompt} key={item}>
+                <p className={styles.text}>{item}</p>
+                <div className={styles.bottom}>
+                  <div className="icon" >
+                    <Copy />
+                  </div>
+                </div>
+              </div>
+            ))
+          )}
         </div>
-        </div>
-        </div>
-        
+      </div>
         <div className={styles.mainRight}>
         <p className={styles.heading}>Your tweet</p>
         <p className={styles.btn}>+Write a tweet</p>
